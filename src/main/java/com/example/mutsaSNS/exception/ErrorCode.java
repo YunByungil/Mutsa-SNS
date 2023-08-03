83cdadd6dc7039ad80b3eb28d4032358e76053ef
YunByungil <dbsquddlfz@naver.com>
2023-08-03 20:54:47 +0900

    feat: not found user, server error 에러 코드 추가

--- src/main/java/com/example/mutsaSNS/exception/ErrorCode.java
+++ src/main/java/com/example/mutsaSNS/exception/ErrorCode.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-    ALREADY_USER_USERNAME(ResultCode.CONFLICT, "이미 존재하는 사용자입니다.");
+    ALREADY_USER_USERNAME(ResultCode.CONFLICT, "이미 존재하는 사용자입니다."),
+    NOT_FOUND_USER(ResultCode.NOT_FOUND, "존재하지 않는 유저입니다."),
+    SERVER_ERROR(ResultCode.INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다.");
 
     private final ResultCode resultCode;
     private final String message;
