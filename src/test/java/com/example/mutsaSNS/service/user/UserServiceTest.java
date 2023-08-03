8e56439f9ea473dd15ed9e5fe681281af5a8bb75
YunByungil <dbsquddlfz@naver.com>
2023-08-03 15:03:56 +0900

    test: 회원가입 중복 아이디 테스트 완료

--- src/test/java/com/example/mutsaSNS/service/user/UserServiceTest.java
+++ src/test/java/com/example/mutsaSNS/service/user/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.example.mutsaSNS.service.user;
 import com.example.mutsaSNS.domain.entity.user.User;
 import com.example.mutsaSNS.domain.repository.user.UserRepository;
 import com.example.mutsaSNS.dto.user.request.UserJoinRequestDto;
+import com.example.mutsaSNS.exception.MutsaSnsAppException;
 import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -37,6 +38,23 @@ class UserServiceTest {
 
         // then
         assertThat(findUser.getUsername()).isEqualTo(username);
+    }
+
+    @DisplayName("회원가입 서비스 중복 검사#validateDuplicateUsername() 테스트")
+    @Test
+    void validateDuplicateUsername() {
+        // given
+        final String username = "아이디";
+        final String password = "비밀번호";
+        UserJoinRequestDto joinDto = createUserJoinDtoWithUsernameAndPassword(username, password);
+
+        // when
+        userService.createUser(joinDto);
+
+        // then
+        assertThatThrownBy(() -> {
+            userService.createUser(joinDto);
+        }).isInstanceOf(MutsaSnsAppException.class);
     }
 
     private UserJoinRequestDto createUserJoinDtoWithUsernameAndPassword(final String username, final String password) {
