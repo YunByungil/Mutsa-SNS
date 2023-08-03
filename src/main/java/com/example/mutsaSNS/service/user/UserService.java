4f046d2df585b321dac0eebf85f45f62d212474f
YunByungil <dbsquddlfz@naver.com>
2023-08-03 15:01:41 +0900

    feat: 회원가입 중복 아이디 검증 로직 추가

--- src/main/java/com/example/mutsaSNS/service/user/UserService.java
+++ src/main/java/com/example/mutsaSNS/service/user/UserService.java
@@ -4,10 +4,14 @@ import com.example.mutsaSNS.domain.entity.user.User;
 import com.example.mutsaSNS.domain.repository.user.UserRepository;
 import com.example.mutsaSNS.dto.user.request.UserJoinRequestDto;
 import com.example.mutsaSNS.dto.user.response.UserJoinResponseDto;
+import com.example.mutsaSNS.exception.ErrorCode;
+import com.example.mutsaSNS.exception.MutsaSnsAppException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.mutsaSNS.exception.ErrorCode.*;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -19,7 +23,16 @@ public class UserService {
 
     @Transactional
     public UserJoinResponseDto createUser(final UserJoinRequestDto joinDto) {
+        validateDuplicateUsername(joinDto.getUsername());
+
         User user = joinDto.toEntity(passwordEncoder.encode(joinDto.getPassword()));
         return new UserJoinResponseDto(userRepository.save(user));
+    }
+
+    private void validateDuplicateUsername(String username) {
+        userRepository.findByUsername(username)
+                .ifPresent(user -> {
+                    throw new MutsaSnsAppException(ALREADY_USER_USERNAME, ALREADY_USER_USERNAME.getMessage());
+                });
     }
 }
