2f20c5040f0d1b42c0df5e54b855dd924b3e5ea6
YunByungil <dbsquddlfz@naver.com>
2023-08-07 17:22:00 +0900

    feat: 자신의 게시글은 좋아요 누를 수 없는 에러 코드 생성

--- src/main/java/com/example/mutsaSNS/exception/ErrorCode.java
+++ src/main/java/com/example/mutsaSNS/exception/ErrorCode.java
@@ -14,9 +14,9 @@ public enum ErrorCode {
     NOT_FOUND_POST(ResultCode.NOT_FOUND, "존재하지 않는 게시글입니다."),
     NOT_FOUND_IMAGE(ResultCode.NOT_FOUND, "존재하지 않는 사진입니다."),
     NOT_FOUND_COMMENT(ResultCode.NOT_FOUND, "존재하지 않는 댓글입니다."),
-
     NOT_MATCH_POST_USER(ResultCode.FORBIDDEN, "게시글을 등록한 유저가 아닙니다."),
     NOT_MATCH_COMMENT_USER(ResultCode.FORBIDDEN, "댓글을 등록한 유저가 아닙니다."),
+    NOT_LIKE_MY_POST(ResultCode.FORBIDDEN, "자신의 게시글은 좋아요를 누를 수 없습니다."),
 
     SERVER_ERROR(ResultCode.INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다.");
 
