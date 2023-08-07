80e417c961c58cfef6f88b41a493ffdcb755233d
YunByungil <dbsquddlfz@naver.com>
2023-08-07 17:26:40 +0900

    feat: 게시글 좋아요 기능 구현
    - 자기 게시글은 좋아요 누를 수 없음.
    - 이미 누른 상태면 좋아요 취소

--- src/main/java/com/example/mutsaSNS/domain/repository/like/PostLikeRepository.java
+++ src/main/java/com/example/mutsaSNS/domain/repository/like/PostLikeRepository.java
@@ -3,5 +3,9 @@ package com.example.mutsaSNS.domain.repository.like;
 import com.example.mutsaSNS.domain.entity.like.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+
+    Optional<PostLike> findByPostIdAndUserId(Long postId, Long userId);
 }
