0b72473d9843f1c29112e678a157bb766b6602dc
YunByungil <dbsquddlfz@naver.com>
2023-08-07 15:35:37 +0900

    feat: 댓글 에러 코드 추가 및 오타 수정

--- src/main/java/com/example/mutsaSNS/exception/ErrorCode.java
+++ src/main/java/com/example/mutsaSNS/exception/ErrorCode.java
@@ -11,10 +11,12 @@ public enum ErrorCode {
     ALREADY_USER_USERNAME(ResultCode.CONFLICT, "이미 존재하는 사용자입니다."),
 
     NOT_FOUND_USER(ResultCode.NOT_FOUND, "존재하지 않는 유저입니다."),
-    NOT_FOUNT_POST(ResultCode.NOT_FOUND, "존재하지 않는 게시글입니다."),
-    NOT_FOUNT_IMAGE(ResultCode.NOT_FOUND, "존재하지 않는 사진입니다."),
+    NOT_FOUND_POST(ResultCode.NOT_FOUND, "존재하지 않는 게시글입니다."),
+    NOT_FOUND_IMAGE(ResultCode.NOT_FOUND, "존재하지 않는 사진입니다."),
+    NOT_FOUND_COMMENT(ResultCode.NOT_FOUND, "존재하지 않는 댓글입니다."),
 
     NOT_MATCH_POST_USER(ResultCode.FORBIDDEN, "게시글을 등록한 유저가 아닙니다."),
+    NOT_MATCH_COMMENT_USER(ResultCode.FORBIDDEN, "댓글을 등록한 유저가 아닙니다."),
 
     SERVER_ERROR(ResultCode.INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다.");
 
